86406f767b839e23e4ca883fdb8f773ce7c706e0
Yoo <tmddls91@inha.edu>
2024-05-16 18:46:41 +0900

    Header commit

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 const Header = () => {
-
+  const navigate = useNavigate();
   return (
     <>
-        <div><button>뒤로 가기</button> <button>메인으로 가기</button></div>
+        <div>
+          <button onClick={()=>navigate(-1)}>
+            뒤로 가기
+          </button> 
+          <Link to = "/Main">
+          <button>
+            메인으로 가기
+          </button>
+          </Link>
+          </div>
     </>
   );
 }
