699e7be79b200742ff4ba419c7b94e220872202f
Yoo <tmddls91@inha.edu>
2024-05-16 18:47:06 +0900

    App commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./containers/Header";
+import Main from "./pages/Main";
+import Menu from "./pages/Menu";
+import Product from "./pages/Product";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -6,6 +12,10 @@ const App = () => {
     <BrowserRouter>
       <Header/>
       <Routes>
+        <Route path = "/main" element={<Main></Main>}></Route>
+        <Route path = "/menu" element={<Menu></Menu>}></Route>
+        <Route path = "/product/:brandName" element={<Product></Product>}></Route>
+        <Route path= "/*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
     </>
